d9ccc0bd3160a1e22d426b376c00678cf408fb94
yogurtshake <59886219+yogurtshake@users.noreply.github.com>
2026-05-28 20:07:54 -0400

    will add extra stats later

--- static/stuff.js
+++ static/stuff.js
@@ -87,8 +87,6 @@ function showRankings() {
                     <td>${row[0]}</td>
                     <td>${row[1].streak}</td>
                     <td>${row[1].clicked_today}</td>
-                    <td>${row[1].total_days_clicked}</td>
-                    <td>${row[1].total_clicks}</td>`;
                 table.appendChild(tr);
             });
         });
